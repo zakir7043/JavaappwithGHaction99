d38bd1f34e525cbf4ddc9addb57192374c0a5685
zakir7043 <159785509+zakir7043@users.noreply.github.com>
2024-04-19 07:04:14 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "azjavaghaction28750-rg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "BookFlight4022"
 }
 
 variable "location" {
